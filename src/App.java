3e212795f04cf35566c0a59c18f621c0b65de256
OlluaS-Code <s.moura.tav@gmail.com>
2026-08-10 15:41:20 -0300

    modify-app.java

--- src/App.java
+++ src/App.java
@@ -1,28 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        String primeiroNome;
-        String segundoNome;
-        String terceiroNome;
-
-        primeiroNome = "Saullo";
-        segundoNome = "Moura";
-        terceiroNome = "Tavares";
-
-        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome);
-        System.out.println(nomeCompleto);
+        System.out.println("Hello, World");
     }
-
-    public static String nomeCompleto(String primeiroNome, String segundoNome) {
-        return nomeCompleto(primeiroNome, segundoNome, "");
-    }
-
-    public static String nomeCompleto(String primeiroNome, String segundoNome, String terceiroNome) {
-        String resultadoParse = "Retorno do Metodo " + primeiroNome.concat(" ").concat(segundoNome);
-
-        if (terceiroNome != null && !terceiroNome.isEmpty()) {
-            resultadoParse = resultadoParse.concat(" ").concat(terceiroNome);
-        }
-
-        return resultadoParse;
-    };
 }
