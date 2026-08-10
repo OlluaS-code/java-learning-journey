36916e7690a9b4c7e347070c14d8fef0f3d78669
OlluaS-Code <s.moura.tav@gmail.com>
2026-08-10 15:08:43 -0300

    implementation-methods-for-concat-names

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String primeiroNome;
+        String segundoNome;
+        String terceiroNome;
+
+        primeiroNome = "Saullo";
+        segundoNome = "Moura";
+        terceiroNome = "Tavares";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome);
+        System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return nomeCompleto(primeiroNome, segundoNome, "");
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome, String terceiroNome) {
+        String resultadoParse = "Retorno do Metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+        if (terceiroNome != null && !terceiroNome.isEmpty()) {
+            resultadoParse = resultadoParse.concat(" ").concat(terceiroNome);
+        }
+
+        return resultadoParse;
+    };
 }
