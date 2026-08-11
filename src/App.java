64937375d0237273060a2eae07d8da07a0d5ad85
OlluaS-Code <s.moura.tav@gmail.com>
2026-08-11 19:01:09 -0300

    add-exec-and-read-arguments-by-cli

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String nome = args[0];
+        String sobrenome = args[1];
+
+        // Conversão (Parsing) dos tipos primitivos usando classes Wrapper
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        // Exibição dos dados formatados no console
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
     }
 }
