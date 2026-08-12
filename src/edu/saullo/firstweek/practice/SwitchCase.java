6dd5ff56158419d47c54c57bd865b8979bc63964
OlluaS-Code <s.moura.tav@gmail.com>
2026-08-12 10:41:54 -0300

    update-switch-case

--- src/edu/saullo/firstweek/practice/SwitchCase.java
+++ src/edu/saullo/firstweek/practice/SwitchCase.java
@@ -20,81 +20,121 @@ public class SwitchCase {
 
     }
 
-    public static void switchWeek() {
+    /*
+     * public static void switchWeek() {
+     * 
+     * String day = "Terça";
+     * 
+     * switch (day) {
+     * case "Segunda":
+     * System.out.println(2);
+     * break;
+     * case "Terça":
+     * System.out.println(3);
+     * break;
+     * case "Quarta":
+     * System.out.println(4);
+     * break;
+     * case "Quinta":
+     * System.out.println(5);
+     * break;
+     * case "Sexta":
+     * System.out.println(6);
+     * break;
+     * case "Sábado":
+     * System.out.println(7);
+     * break;
+     * case "Domingo":
+     * System.out.println(1);
+     * break;
+     * 
+     * default:
+     * System.out.println("Dia Inválido!");
+     * break;
+     * }
+     * }
+     */
 
+    public static void switchWeek() {
         String day = "Terça";
 
         switch (day) {
-            case "Segunda":
-                System.out.println(2);
-                break;
-            case "Terça":
-                System.out.println(3);
-                break;
-            case "Quarta":
-                System.out.println(4);
-                break;
-            case "Quinta":
-                System.out.println(5);
-                break;
-            case "Sexta":
-                System.out.println(6);
-                break;
-            case "Sábado":
-                System.out.println(7);
-                break;
-            case "Domingo":
-                System.out.println(1);
-                break;
-
-            default:
-                System.out.println("Dia Inválido!");
-                break;
+            case "Segunda" -> System.out.println(2);
+            case "Terça" -> System.out.println(3);
+            case "Quarta" -> System.out.println(4);
+            case "Quinta" -> System.out.println(5);
+            case "Sexta" -> System.out.println(6);
+            case "Sábado" -> System.out.println(7);
+            case "Domingo" -> System.out.println(1);
+            default -> System.out.println("Dia Inválido!");
         }
     }
 
-    public static void switchNumber() {
+    /*
+     * public static void switchNumber() {
+     * 
+     * byte switchNumber = 6;
+     * 
+     * switch (switchNumber) {
+     * case 1:
+     * 
+     * case 2:
+     * 
+     * case 3:
+     * System.out.println("Certo");
+     * break;
+     * case 4:
+     * System.out.println("Errado");
+     * break;
+     * case 5:
+     * System.out.println("Talvez");
+     * break;
+     * 
+     * default:
+     * System.out.println("Número Inválido!");
+     * break;
+     * }
+     * }
+     */
 
+    public static void switchNumber() {
         byte switchNumber = 6;
 
         switch (switchNumber) {
-            case 1:
-
-            case 2:
-
-            case 3:
-                System.out.println("Certo");
-                break;
-            case 4:
-                System.out.println("Errado");
-                break;
-            case 5:
-                System.out.println("Talvez");
-                break;
-
-            default:
-                System.out.println("Número Inválido!");
-                break;
+            case 1, 2, 3 -> System.out.println("Certo");
+            case 4 -> System.out.println("Errado");
+            case 5 -> System.out.println("Talvez");
+            default -> System.out.println("Número Inválido!");
         }
     }
+
+    /*
+     * public static void switchHolidays(String month) {
+     * 
+     * String chosenMonth = month;
+     * 
+     * switch (chosenMonth) {
+     * 
+     * case "Janeiro":
+     * 
+     * case "Julho":
+     * 
+     * case "Dezembro":
+     * System.out.println("Estou de Férias");
+     * break;
+     * 
+     * default:
+     * System.out.println("Estou Trabalhando!");
+     * break;
+     * }
+     * }
+     */
 
     public static void switchHolidays(String month) {
-
-        String chosenMonth = month;
-
-        switch (chosenMonth) {
-
-            case "Janeiro":
-
-            case "Julho":
-
-            case "Dezembro":
-                System.out.println("Estou de Férias");
-                break;
-
-            default:
-                System.out.println("Estou Trabalhando!");
-                break;
+        switch (month) {
+            case "Janeiro", "Julho", "Dezembro" -> System.out.println("Estou de Férias");
+            default -> System.out.println("Estou Trabalhando!");
         }
     }
+
 }
